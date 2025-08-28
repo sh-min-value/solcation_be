5224dd3e8432da22c4f1f25d62fdfa2c06fca283
subin930 <bread0930@g.skku.edu>
2025-08-28 23:42:51 +0900

    Feat: initial commit

---
+++ src/main/java/org/solcation/solcation_be/SolcationBeApplication.java
@@ -0,0 +1,13 @@
+package org.solcation.solcation_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolcationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolcationBeApplication.class, args);
+    }
+
+}
