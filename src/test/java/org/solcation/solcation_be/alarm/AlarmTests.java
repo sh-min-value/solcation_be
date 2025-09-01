6977bb3a1497bb41ce9be392caca4f73811b8f78
subin930 <bread0930@g.skku.edu>
2025-09-01 12:35:05 +0900

    Feat: Test 파일들 추가

---
+++ src/test/java/org/solcation/solcation_be/alarm/AlarmTests.java
@@ -0,0 +1,10 @@
+package org.solcation.solcation_be.alarm;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class AlarmTests {
+
+}
