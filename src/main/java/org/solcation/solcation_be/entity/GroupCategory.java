25c912320bce34eb2870b82d0300e0b654fbfad2
subin930 <bread0930@g.skku.edu>
2025-09-02 12:29:42 +0900

    Fix: Group Repository/Entity 수정

---
+++ src/main/java/org/solcation/solcation_be/entity/GroupCategory.java
@@ -0,0 +1,25 @@
+package org.solcation.solcation_be.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "group_catgory_tb")
+public class GroupCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gcPk;
+
+    @Column(name = "gc_name", nullable = false)
+    private String gcName;
+
+    @Column(name = "gc_icon", nullable = false)
+    private String gcIcon;
+}
