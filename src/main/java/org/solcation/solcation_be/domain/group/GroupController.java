536364759912c14a84e5a7420fd4dc2d76410a94
subin930 <bread0930@g.skku.edu>
2025-09-04 10:48:00 +0900

    Feat: 그룹 - 스웨거 상 엔드포인트 설명 추가

--- src/main/java/org/solcation/solcation_be/domain/group/GroupController.java
+++ src/main/java/org/solcation/solcation_be/domain/group/GroupController.java
@@ -1,5 +1,6 @@
 package org.solcation.solcation_be.domain.group;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,17 +26,20 @@ public class GroupController {
     private final UserRepository userRepository;
     private final GroupService groupService;
 
+    @Operation(summary = "새 그룹 생성")
     @PostMapping(value = "/new", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public boolean addGroup(@ModelAttribute AddGroupReqDTO addGroupReqDTO, @AuthenticationPrincipal JwtPrincipal user){
         User groupLeader = userRepository.findByUserId(user.userId()).orElseThrow(() -> new CustomException(ErrorCode.UNAUTHORIZED));
         return groupService.addGroup(addGroupReqDTO, groupLeader);
     }
 
+    @Operation(summary = "그룹 목록 조회")
     @GetMapping("/list")
     public List<GroupListDTO> getList(@AuthenticationPrincipal JwtPrincipal user, @RequestParam(required = false) String searchTerm) {
         return groupService.getList(user.userId(), searchTerm);
     }
 
+    @Operation(summary = "그룹 메인 - 그룹 정보 조회")
     @GetMapping("/{groupId:\\d+}/get")
     public GroupInfoDTO groupMain(@PathVariable("groupId") Long groupId) {
         return groupService.getGroupInfo(groupId);
