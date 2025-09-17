4d82258e40823c67b1f148711a1913e45607324a
sechang <1031choisechang@gmail.com>
2025-09-17 18:50:44 +0900

    Feat:인사이트 출력 DTO 생성

---
+++ src/main/java/org/solcation/solcation_be/domain/stats/dto/InsightDTO.java
@@ -0,0 +1,11 @@
+package org.solcation.solcation_be.domain.stats.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InsightDTO {
+    private String msg;
+    private Long tpPk;
+}
